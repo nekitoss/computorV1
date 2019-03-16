0a68198f93d8bb1d987e5438a2dc3924a4f5a91b
Nick <nekit_p@ukr.net>
2019-03-16 14:56:38 +0200

    check  and numbers

--- src/com/nekitoss/Eq.java
+++ src/com/nekitoss/Eq.java
@@ -4,8 +4,6 @@ package com.nekitoss;
  * Created by mpochuka on 3/16/19.
  */
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,9 +11,32 @@ public class Eq {
     private String full_eq;
 
 
+    private Pattern eqCheckPattern = Pattern.compile("=");
+    private Pattern numbersAroundCheckPattern = Pattern.compile(".*\\d.*=.*\\d.*");
+
+
     public Eq(String full_eq) {
         this.full_eq = full_eq.replaceAll(" ", "");
     }
 
+    public boolean checkEqualsSign(){
+        Matcher mEq = eqCheckPattern.matcher(full_eq);
+        int count = 0;
 
+        while (mEq.find())
+            count++;
+
+        if (count == 0)
+            Msg.errMsgExit("Equation has no \"=\"sign!");
+        else if (count > 1)
+            Msg.errMsgExit("Equation can have only one \"=\"sign!");
+        else
+        {
+            Matcher mNum = numbersAroundCheckPattern.matcher(full_eq);
+            if (!mNum.find())
+                Msg.errMsgExit("Missing numbers around \"=\"");
+            return true;
+        }
+        return false;
+    }
 }
